596cf52b196abe27e2bf517deae4a6b73b48836b
sonnguyen-gw <son.nguyen@globalway.co.jp>
2016-12-08 15:01:20 +0900

    Init verison

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//const App = () => <h1>Hello World!</h1>
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello from extended Component</h1>
+				<h2>..and a sibling line</h2>
+			</div>
+			)
+	}
+}
+
+export default App;
